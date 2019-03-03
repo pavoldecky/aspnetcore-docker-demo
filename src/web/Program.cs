d6db5df8da65a872ab7a580b3b59590e128c329a
Pavol Decky <pavol.decky@quadrotech-it.com>
2019-03-03 13:04:01 +0100

    Add docker support

--- src/web/Program.cs
+++ src/web/Program.cs
@@ -19,6 +19,7 @@ namespace web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://+:80")
                 .UseStartup<Startup>();
     }
 }
