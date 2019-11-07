ebde78c51a730067778108b04ee25d63e8d714aa
Pavol Decky <pavol.decky@quadrotech-it.com>
2019-11-07 07:53:15 +0100

    Migrate to ASP.NET Core 3

--- src/web/Startup.cs
+++ src/web/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 
 namespace web
 {
@@ -18,7 +20,7 @@ namespace web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
